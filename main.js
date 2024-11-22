034236ce3397cb1eefa2aaa0d3663bf88b209f68
Dwaipayan Biswas <biswastatay73@gmail.com>
2024-11-22 11:06:00 +0530

    updated

--- main.js
+++ main.js
@@ -1,9 +1,50 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+const { config } = require("dotenv");
+config()
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
 
-const genAI = new GoogleGenerativeAI("AIzaSyBXi7QsSUS2Tj4U_4G0NgfBFMmeu1ZrlmY");
+app.use(express.json())
+app.use(bodyParser.json())
+
+app.get('/', (req, res)=>{
+    res.send("Hello, GEMINI!")
+})
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-const prompt = "Explain how AI works";
+// const prompt = "value of pie in math";
 
-const result = await model.generateContent(prompt);
-console.log(result.response.text());
+const generate = async(prompt) =>{
+    try {
+        const result = await model.generateContent(prompt);
+        // console.log(result.response.text());
+        return result.response.text();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// generate();
+
+app.get('/api/content', async (req, res) => {
+
+    try {
+
+        const data = req.body.prompt;
+        const result = await generate(data);
+        res.send({
+            "result":result
+        })
+
+    } catch (error) {
+        console.log(error)
+    }
+
+})
+
+app.listen(3000, (req,res)=>{
+    console.log("Server is Running 3000")
+})
